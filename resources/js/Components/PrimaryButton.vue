12edbc5ffbf42bc779899658b93eb2bb12220d7a
azaf70 <abdullahzafar70@gmail.com>
2023-08-26 03:37:53 +0100

    added functionality to dynamically generate rows and columns based on user's input

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center px-4 bg-green-500 dark:bg-gray-200 border border-transparent rounded-md font-semibold py-2.5 text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-green-600 dark:hover:bg-white focus:bg-green-600 dark:focus:bg-white focus:outline-none focus:ring-4 focus:ring-green-600 focus:ring-offset-2 transition ease-in-out duration-150"
+        class="inline-flex items-center px-4 bg-green-500 dark:bg-gray-200 border border-transparent rounded-md font-semibold py-2.5 text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-green-600 dark:hover:bg-white focus:bg-green-600 dark:focus:bg-white focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-1 transition ease-in-out duration-150"
     >
         <slot />
     </button>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,7 @@
             :class="{ 'hidden': !isTableOpen}"
             class="flex  z-50 p-4 top-0 left-0 right-0 fixed items-center justify-center overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)]"
         >
-            <div class="m-auto w-full max-w-md max-h-full">
+            <div class="m-auto w-full max-w-4xl max-h-full">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <button
@@ -51,6 +51,102 @@
                         <p class="text-sm font-normal text-gray-500 dark:text-gray-400">
                             Please choose the number of rows and columns
                         </p>
+                        <div class="flex my-2 gap-8">
+                            <div>
+                                <InputLabel
+                                    for="columns"
+                                    value="Columns"
+                                />
+                                <TextInput
+                                    id="columns"
+                                    v-model="newColumns"
+                                    type="number"
+                                    class="mt-1 block w-full p-2"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <InputLabel
+                                    for="rows"
+                                    value="Rows"
+                                />
+                                <TextInput
+                                    id="rows"
+                                    v-model="newRows"
+                                    type="number"
+                                    class="mt-1 block w-full p-2"
+                                />
+                            </div>
+                            <div>
+                                <PrimaryButton
+                                    class="mt-6"
+                                    @click.prevent="buildTable"
+                                >
+                                    <font-awesome-icon
+                                        :icon="isTableGenerating ? 'circle-notch' : 'table'"
+                                        class="p-0.5"
+                                        :spin="isTableGenerating"
+                                    />
+                                </PrimaryButton>
+                            </div>
+                        </div>
+                        <p class="text-sm pb-2 font-normal text-gray-500 dark:text-gray-400">
+                            Rows can be added later dynamically
+                        </p>
+                        <div
+                            v-if="showGeneratedTable"
+                            class="py-2"
+                        >
+                            <div class="relative overflow-auto shadow-md sm:rounded-lg">
+                                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th
+                                                v-for="(column, index) in columns"
+                                                :key="index"
+                                                scope="col"
+                                                class="px-6 py-3"
+                                            >
+                                                <TextInput
+                                                    id="columns"
+                                                    v-model="form.columnNames[index]"
+                                                    class="mt-1 block w-full p-2"
+                                                    required
+                                                />
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr
+                                            v-for="(row, rowIndex) in rows"
+                                            :key="rowIndex"
+                                            class="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+                                        >
+                                            <td
+                                                v-for="(column, columnIndex) in columns"
+                                                :key="columnIndex"
+                                                class="px-6 py-4 whitespace-nowrap"
+                                            >
+                                                <TextInput
+                                                    id="columns"
+                                                    v-model="form.rowNames[columnIndex][rowIndex]"
+                                                    class="mt-1 block w-full p-2"
+                                                    required
+                                                />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="my-2">
+                                <PrimaryButton
+                                    class="mt-6"
+                                    @click.prevent="submitTableData"
+                                >
+                                    Create Table
+                                </PrimaryButton>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -74,12 +170,52 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { ref } from 'vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const isTableOpen = ref(false);
+const newColumns = ref();
+const newRows = ref();
+const columns = ref([]);
+const rows = ref([]);
+const isTableGenerating = ref(false);
+const showGeneratedTable = ref(false);
+
+const form = useForm({
+    columns: [],
+    rows: [],
+    columnNames: [],
+    rowNames: [],
+});
 function openTableBuilder() {
     isTableOpen.value = true;
+}
+
+function buildTable() {
+    isTableGenerating.value = true;
+    columns.value = [];
+    rows.value = [];
+    form.columns = [];
+    form.rows = [];
+    for (let i = 1; i <= newColumns.value; i++) {
+        columns.value.push(`Column ${i + 1}`);
+        form.columnNames.push('');
+    }
+    for (let i = 1; i <= newRows.value; i++) {
+        rows.value.push(`${i + 1}`);
+        form.rowNames.push([]);
+    }
+    showGeneratedTable.value = true;
+    setTimeout(() => {
+        isTableGenerating.value = false;
+    }, 500);
+}
+
+function submitTableData() {
+    console.log(form);
 }
 </script>
